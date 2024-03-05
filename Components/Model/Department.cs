36b419ee21c021caa5a411f72e9273174c2f161a
Vaughn Pieterse <vpieterse@fvc.co.za>
2024-03-05 10:03:53 +0200

    Add project files.

---
+++ Components/Model/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeManagementSystem.Components.Model
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ Components/Service/EmployeeService.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeManagementSystem.Components.Model
+{
+    public class EmployeeService
+    {
+        private List<Employee> employees;
+
+        public EmployeeService()
+        {
+            employees = new List<Employee>
+            {
+                new Employee { Id = 1, FirstName = "John", LastName = "Doe", Email = "john@example.com", HireDate = DateTime.Now, DepartmentId = 101 },
+                new Employee { Id = 2, FirstName = "Jane", LastName = "Smith", Email = "jane@example.com", HireDate = DateTime.Now, DepartmentId = 102 },
+            };
+        }
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+
+        public Employee GetEmployeeById(int id)
+        {
+            return employees.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
